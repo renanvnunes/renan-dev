dd6c9e79a684c0ba913418c719db48fa4b6b55be
Renan Nunes <renan.dev@hotmail.com>
2022-07-05 19:51:15 -0300

    Change colors

--- components/Projeto.vue
+++ components/Projeto.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="projeto">
-		Projeto criado no <a href="https://ubuntu.com" target="_blank" class="ubuntu"><b>Ubuntu OS</b></a> e <a class="vscode" href="https://code.visualstudio.com" target="_blank"><b>VSCode</b></a>, com 
+		Projeto criado no <a href="https://ubuntu.com" target="_blank" class="ubuntu"><b>Ubuntu OS</b></a> e <a class="vscode" href="https://code.visualstudio.com" target="_blank"><b>VSCode</b></a>, utilizando 
 		<a class="vue" href="https://vuejs.org" target="_blank"><b>Vue.js</b></a>+
-		<a class="nuxt" href="https://nuxtjs.org" target="_blank"><b>NuxtJS</b></a> - Veja o código no <a href=""><b>Github</b></a>
+		<a class="nuxt" href="https://nuxtjs.org" target="_blank"><b>NuxtJS</b></a>. Deploy feito na <a href="https://vercel.com" target="_blank"><b>Vercel</b></a> - Veja o código no <a href="https://github.com/renanvnunes/renan-dev" target="_blank"><b>Github</b></a>
 	</div>	
 </template>
